0740f6c7ef74d751f8f32d8608e5a23f483dcc15
IlySmall <75866157+IlySmall@users.noreply.github.com>
2021-03-25 01:17:57 +0100

    obama

--- commands/obama.js
+++ commands/obama.js
@@ -7,7 +7,7 @@ module.exports = {
     description: 'Haha obama. Idea by MLG2460',
     usage:"mental breakdown",
 	execute(message) {
-        var array = ["https://cdn.discordapp.com/attachments/739921627558445206/779889456450502696/obama.gif", "https://cdn.discordapp.com/attachments/739921627558445206/779889786810925086/obama-prism-icegif.gif", "https://cdn.discordapp.com/attachments/739921627558445206/779892131358965810/Obamahedron.gif", "https://cdn.discordapp.com/attachments/739921627558445206/773289668723867690/obama-corn.gif"]
+        var array = ["https://cdn.discordapp.com/attachments/739921627558445206/779889456450502696/obama.gif", "https://static.wikia.nocookie.net/fandomium/images/0/0c/Obomba.gif/revision/latest?cb=20191122172620", "https://cdn.discordapp.com/attachments/739921627558445206/824436485948047360/13e.png", "https://i.kym-cdn.com/photos/images/original/001/548/017/98a.gif", "https://i.kym-cdn.com/photos/images/original/001/548/021/341.gif"]
         message.channel.send(array[getRandomInt(array.length-1)]); 
 	},
 };
