5147f0f83bb656754978dc10cbe790889204b7e0
IlySmall <75866157+IlySmall@users.noreply.github.com>
2021-03-27 18:50:20 +0100

    unretarded the avatar also gitignore the cfg

--- commands/avatar.js
+++ commands/avatar.js
@@ -4,11 +4,11 @@ module.exports = {
 	aliases: ['icon', 'pfp'],
 	execute(message) {
 		if (!message.mentions.users.size) {
-			return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ dynamic: true })}>`);
+			return message.channel.send(`Your avatar: ${message.author.displayAvatarURL({ dynamic: true, format: "png", size: 1024 })}`);
 		}
 
 		const avatarList = message.mentions.users.map(user => {
-			return `${user.username}'s avatar: <${user.displayAvatarURL({ dynamic: true })}>`;
+			return `${user.username}'s avatar: ${user.displayAvatarURL({ dynamic: true, format: "png", size: 1024 })}`;
 		});
 
 		message.channel.send(avatarList);
