a2211ef7d50ad57e3e38ce5864cc74c582ec4fb1
IlySmall <75866157+IlySmall@users.noreply.github.com>
2021-04-06 20:59:15 +0200

    fixed volume tm

--- commands/volume.js
+++ commands/volume.js
@@ -3,7 +3,7 @@
     description: 'music',
     usage:" ",
 	execute(message,args,b,c,distube) {
-        if(100>args[0]>1){
+        if(100>parseFloat(args[0])>1){
             distube.setVolume(message, args[0])
             message.channel.send("Volume set to `"+args[0]+"%`");
         }
