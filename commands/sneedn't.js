e4c4aa8c02b431b7280f5d0ff5a11c377a9c2ee3
IlySmall <75866157+IlySmall@users.noreply.github.com>
2021-04-17 21:47:46 +0200

    Update sneedn't.js

--- commands/sneedn't.js
+++ commands/sneedn't.js
@@ -7,7 +7,7 @@ module.exports = {
     permissions: "admin global",
     execute(message, args, content) {
         if(args[0]=="list"){
-            var reply
+            var reply = "";
             fs.readFile('sneed.json', function (err, data) {
                 if (err) {
                     throw err;
