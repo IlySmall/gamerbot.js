6ca1aede9b128a8d36e1b7f011d3b4a20f572023
IlySmall <75866157+IlySmall@users.noreply.github.com>
2021-06-02 17:49:17 +0200

    quick and dirty shuffle implementation

---
+++ commands/shuffle.js
@@ -0,0 +1,13 @@
+module.exports = {
+	name: 'shuffle',
+    description: 'music',
+    usage:" ",
+    guildOnly: true,
+	execute(message,a,b,c,distube) {
+        try{distube.shuffle(message);}
+        catch(e){
+            console.log(e)
+            message.channel.send("You have to be in a voice channel or there is an error.")
+        }
+	},
+};
