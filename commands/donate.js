c109508a5d49b80b75035b03eb40ed9b700b9942
IlySmall <75866157+IlySmall@users.noreply.github.com>
2021-06-09 18:30:03 +0200

    Create donate.js

---
+++ commands/donate.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'donate',
+    description: 'pay my creator so he can most likely spend it all on car parts, gasoline or electronics lmao',
+	execute(message) {
+        message.channel.send(`If you like my fucking face enough and want to support the retard that coded me you now can here: https://paypal.me/nowabi \n he's probably gonna spend it all on electronics or car parts btw`);
+	},
+};
