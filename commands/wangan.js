b205c94a89dc44ffb4c10f09f94460918412275d
IlySmall <75866157+IlySmall@users.noreply.github.com>
2021-06-09 18:13:03 +0200

    Create wangan.js

---
+++ commands/wangan.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'wangan',
+    description: 'posts the entirety of fucking wangan midnight',
+	execute(message) {
+        message.channel.send(`https://www.youtube.com/watch?v=P7YC60yJJsE`);
+	},
+};
