c164e6d7de8f16d2fde1a7eb6259520189785fa5
IlySmall <75866157+IlySmall@users.noreply.github.com>
2021-06-13 00:50:01 +0200

    monty

---
+++ commands/grail.js
@@ -0,0 +1,8 @@
+module.exports = {
+	name: 'grail',
+    description: 'Monty Python and the Holy Grail',
+    usage:" ",
+	execute(message) {
+        message.channel.send(`https://cdn.discordapp.com/attachments/639410195528679424/852664115113492540/MontyPythonAndTheHolyGrail.webm`);
+	},
+};
