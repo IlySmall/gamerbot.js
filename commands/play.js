fe0c6970a6436d6ca48be1a433169dd7c51d1e70
IlySmall <75866157+IlySmall@users.noreply.github.com>
2021-06-28 00:13:56 +0200

    better error handling

--- commands/play.js
+++ commands/play.js
@@ -5,10 +5,9 @@ module.exports = {
     aliases: ['p'],
     guildOnly: true,
 	execute(message,args,b,c,distube) {
-        try{distube.play(message, args.join(" "));}
-        catch(e){
-            console.log(e)
-            message.channel.send("You have to be in a voice channel or there is an error.")
-        }
-	},
+        distube.play(message, args.join(" ")).catch((e) => {
+            console.error(e)
+            message.channel.send("There has been this: `"+e+"` error but I am most likely still alive, just try again")
+        });
+    },
 };
