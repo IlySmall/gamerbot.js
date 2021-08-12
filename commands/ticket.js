b53d71fe6ab49c38e41a7ffb0930235ea3169ea1
IlySmall <75866157+IlySmall@users.noreply.github.com>
2021-08-12 02:01:12 +0200

    fuck you discord you're shit

--- commands/ticket.js
+++ commands/ticket.js
@@ -6,8 +6,7 @@ module.exports = {
 	execute(message,args,content) {
         var isArgsContent=args[0]==content
         var topic = (isArgsContent ? args[0]:args[0]+content)
-        var ticketname=message.author.tag+"_"+Date.now()
-        message.channel.guild.channels.create(`${ticketname}`, {
+        message.channel.guild.channels.create(`${message.author.tag}_${Date.now()}`, {
             type: 'text',
             parent: '852629180009152533',
             topic: topic,
@@ -31,7 +30,6 @@ module.exports = {
               }
             ],
           })
-        message.channel.send("Ticket created.")
-        message.channel.guild.channels.cache.find(channel=>channel.name===ticketname).send(`${message.author.toString()}, the ticket is **here**.`)
+        message.reply("Ticket created, look in the modmail category.")
 	},
 };
