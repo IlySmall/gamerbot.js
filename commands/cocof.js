1762066f576f447d25f8829d6102c6d3f68ae5c6
IlySmall <75866157+IlySmall@users.noreply.github.com>
2021-08-19 20:30:13 +0200

    funnies

--- commands/cocof.js
+++ commands/cocof.js
@@ -1,6 +1,6 @@
 module.exports = {
-	name: 'faggot',
-    description: 'calls someone a faggot, video and idea by ワシ#5055',
+	name: 'cocof',
+    description: 'coco calls someone a faggot, video and idea by ワシ#5055',
     usage:" ",
 	execute(message) {
         message.channel.send(`<@${message.author.id}>: https://streamable.com/8tmjld`);
---
+++ commands/event.js
@@ -0,0 +1,20 @@
+const { MessageEmbed } = require('discord.js');
+module.exports = {
+	name: 'event',
+	description: 'Announce an event. Restricted to the Event Organizer role.',
+	guildOnly: true,
+    cooldown:300,
+	execute(message, args, content, client) {
+		if(!message.member.roles.cache.has("875196025197969488")) return message.reply("You can't use that command.")
+        var embed = new MessageEmbed()
+            .setColor('#59bfff')
+            .setTitle('New event!')
+            .setAuthor(message.author.tag, message.author.avatarURL)
+            .setThumbnail(message.author.avatarURL)
+            .setDescription("<@&875033404071084112>: "+content)
+            .addField('Time:',`<t:${args[0]}:F>`,true)
+            .setTimestamp()
+            .setFooter('Please donate at https://paypal.me/nowabi if you like the bot');
+        client.guilds.cache.get("716993263889809510").channels.cache.get("877947724392169503").send(embed)
+	},
+};
