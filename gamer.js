e7497ebb39be022e4e35463d0f20039d4cabec32
IlySmall <75866157+IlySmall@users.noreply.github.com>
2021-11-08 09:10:33 +0100

    rrat now has ping

--- gamer.js
+++ gamer.js
@@ -78,7 +78,10 @@ client.on('ready', () => { // Stuff that happens when the bot alives
 });
 
 client.on('messageCreate', message => {
-    if (message.content=="ogey") message.reply("rrat");
+    if (message.content=="ogey"){
+        var rratping=Date.now()-message.createdTimestamp
+        message.reply("rrat("+rratping+"ms)");
+    }
     //sneed vibe check
     fs.readFile('sneed.json', function (err, data) {
         if (err) {
